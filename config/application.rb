4bed3fda3a3bc7a440e6a0fd8ef35a22ff090fd3
Sehong Park <sehong.park86@gmail.com>
2018-03-17 05:21:04 +0900

    add Docker module

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Judge
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{config.root}/lib) # add this line
   end
 end
