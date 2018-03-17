d110f929a31f1771273e1b6ed5cbab9da234d277
Sehong Park <sehong.park86@gmail.com>
2018-03-17 09:50:48 +0900

    mutex docker module

--- lib/docker.rb
+++ lib/docker.rb
@@ -1,24 +1,21 @@
 # /lib/docker.rb
 require 'open3'
+require 'thread'
 
 module Docker
+  # class variable
+  @@semaphore = Mutex.new
 
   def hello
     "hello"
   end
 
-  def ls
-    Open3.capture3("docker exec judge ls /root")
-  end
-
-#  def self.save_code(code)
-#    "/root/code.txt"
-#  end
-
   # class method
   def self.judge(code_path, lang)
     # !가 들어가면 문제가 생김.. bash에서 특별한 의미로 사용..
-    Open3.capture3("docker exec judge run.sh #{code_path} #{lang}")
+    semaphore.synchronize {
+      Open3.capture3("docker exec judge run.sh #{code_path} #{lang}")
+    }
   end
 
   def self.save_code(code)
