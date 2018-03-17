0a1239b8c76c016efbaec5ac281a3acb6a4247c6
Sehong Park <sehong.park86@gmail.com>
2018-03-17 09:56:20 +0900

    mutex docker module

--- lib/docker.rb
+++ lib/docker.rb
@@ -13,6 +13,7 @@ module Docker
   # class method
   def self.judge(code_path, lang)
     # !가 들어가면 문제가 생김.. bash에서 특별한 의미로 사용..
+    # compgen -k에서 나오는 키워드들 문제 안생기게 해야할 듯??
     @@semaphore.synchronize {
       Open3.capture3("docker exec judge run.sh #{code_path} #{lang}")
     }
